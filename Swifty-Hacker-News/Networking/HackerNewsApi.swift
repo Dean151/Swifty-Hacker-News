4d296ccf624c02b2f6b25a364399aba538882832
Dean151 <thomas.durand.verjat@gmail.com>
2017-12-11 09:03:36 +0100

    Beginning API file

--- Swifty-Hacker-News/Networking/HackerNewsApi.swift
+++ Swifty-Hacker-News/Networking/HackerNewsApi.swift
@@ -1,9 +1,22 @@
 //
-//  HackerNewApi.swift
+//  HackerNewsApi.swift
 //  Swifty-Hacker-News
 //
 //  Created by Thomas Durand on 10/12/2017.
 //  Copyright © 2017 Thomas Durand. All rights reserved.
 //
 
+import Alamofire
 import Foundation
+
+class HackerNewsApi {
+    
+    let baseUrl = URL("https://hacker-news.firebaseio.com/v0/")!
+    
+    enum Ranking: String {
+        case topstories
+        case newstories
+        case beststories
+    }
+    
+}
