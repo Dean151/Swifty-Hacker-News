40f07865501d2b3262abeb5427d339e595260ad1
Dean151 <thomas.durand.verjat@gmail.com>
2017-12-11 09:02:59 +0100

    Moving things around

---
+++ Swifty-Hacker-News/Networking/HackerNewsApi.swift
@@ -0,0 +1,9 @@
+//
+//  HackerNewApi.swift
+//  Swifty-Hacker-News
+//
+//  Created by Thomas Durand on 10/12/2017.
+//  Copyright © 2017 Thomas Durand. All rights reserved.
+//
+
+import Foundation
